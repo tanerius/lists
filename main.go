091ff51e0ad0151b70e222f9546d5cb2f27276cb
Taner Selim <taner,selim@gameforge.com>
2024-05-09 20:56:38 +0200

    some examples

--- main.go
+++ main.go
@@ -7,15 +7,33 @@ import (
 )
 
 func main() {
+	// create a limimted size queue
 	testq := lists.NewQueue[int](5)
+
 	testq.Enqueue(5)
 	testq.Enqueue(6)
 	testq.Enqueue(7)
+	testq.Enqueue(8)
+	testq.Enqueue(9)
+	testq.Enqueue(10)
 
 	fmt.Println(testq.Dequeue())
 	fmt.Println(testq.Dequeue())
 	fmt.Println(testq.Dequeue())
 	fmt.Println(testq.Dequeue())
+	fmt.Println(testq.Dequeue())
+	fmt.Println(testq.Dequeue())
+
+	/*
+		OUTPUT:
+
+		6 <nil>
+		7 <nil>
+		8 <nil>
+		9 <nil>
+		10 <nil>
+		0 empty list
+	*/
 
 	tests := lists.NewStack[int]()
 	tests.Push(1)
